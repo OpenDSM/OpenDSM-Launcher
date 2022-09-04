12f143294fbc2a5832830893c0d5fa68e10a524e
dcmanproductions <dcmanproductions@gmail.com>
2022-09-04 09:41:30 -0400

    async load product items

--- src/assets/js/home-page.js
+++ src/assets/js/home-page.js
@@ -1,11 +1,21 @@
-async function LoadElements(){
-    
+async function LoadElements() {
+    LoadCarouselItem("latest")
+    LoadCarouselItem("popular")
 }
 
-async function LoadPopular(){
-
-}
-
-async function LoadLatest(){
-    let response = await fetch(`${host}/api/product`)
+async function LoadCarouselItem(type) {
+    let element = $(`#${type}-products.carousel`)[0];
+    let response = await APICall("product", type, "GET", { page: 0, count: 10 });
+    if (response.ok) {
+        let json = await response.json();
+        let jarray = Array.from(json);
+        if (jarray.length != 0) {
+            for (let i = jarray.length; i == 0; i--) {
+                element.appendChild(CreateProductElement(jarray[i].id))
+                element.style.display = "";
+                return;
+            }
+        }
+    }
+    element.remove();
 }
