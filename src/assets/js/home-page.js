204dc4871de567b62761336a3f32c7d4059f26bc
dcmanproductions <dcmanproductions@gmail.com>
2022-09-04 20:42:27 -0400

    Added check for is developer

--- src/assets/js/home-page.js
+++ src/assets/js/home-page.js
@@ -1,3 +1,13 @@
+LoadElements()
+
+if (!user.git.isDeveloperAccount) {
+    $("#submit-product").attr('controller', '')
+    $("#submit-product").attr('name', '')
+    $("#submit-product").on('click', () => {
+        new ActivateDeveloperAccountPopup().open();
+    })
+}
+
 async function LoadElements() {
     LoadCarouselItem("latest")
     LoadCarouselItem("popular")
@@ -5,17 +15,14 @@ async function LoadElements() {
 
 async function LoadCarouselItem(type) {
     let element = $(`#${type}-products.carousel`)[0];
-    let response = await APICall("product", type, "GET", { page: 0, count: 10 });
+    let response = await APICall("product", "", "GET", { type, page: 0, count: 10 });
     if (response.ok) {
         let json = await response.json();
-        let jarray = Array.from(json);
-        if (jarray.length != 0) {
-            for (let i = jarray.length; i == 0; i--) {
-                element.appendChild(CreateProductElement(jarray[i].id))
-                element.style.display = "";
-                return;
-            }
-        }
+        json.forEach(async i => {
+            $(`#${type}-products.carousel .list.horizontal`)[0].appendChild(await CreateProductElement(i.id))
+            element.style.display = "";
+        })
+        return;
     }
     element.remove();
 }
